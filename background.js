3d60134bdd1e3a90ce9950c5e01b1cc74399b7c4
AlricLobo <39924933+AlricLobo@users.noreply.github.com>
2019-01-03 14:39:51 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
 //hello world
+
+// Constants
+var Mini_Vidy = 'miniVidy';
+var MINI_SCREEN_LAST_TOP = 'miniScreenLastTop';
+var MINI_SCREEN_LAST_LEFT = 'miniScreenLastLeft';
+var MINI_SCREEN_LAST_HEIGHT = 'miniScreenLastHeight';
+var MINI_SCREEN_LAST_WIDTH = 'miniScreenLastWidth';
+var miniVidy = true;
